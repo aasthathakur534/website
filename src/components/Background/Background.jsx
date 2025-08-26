56f68cb7be0eecd601d2e70d1083851cdc559351
aasthathakur534 <aasthakur1947@gmail.com>
2025-08-26 23:02:31 +0530

    feat: add folders

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,18 @@
+import "./Background.css"
+import video1 from '../../Assets/video1.mp4'
+import image1 from '../../Assets/image1.png'
+import image2 from '../../Assets/image1.png'
+import image3 from '../../Assets/image1.png'
+
+
+
+const Background = () => {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Background
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import "./Hero.css"
+
+const Hero = () => {
+    let heroData = [
+        {text1:"Dive into",text2:"what you love"},
+        {text1:"Indulge",text2:"your passions"},
+        {text1:"Give in to",text2:"your passions"}
+    ]
+    const [heroCount,setHeroCount] = useState(2);
+    const [playStatus,setPlayStatus] = useState(false);
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount = {heroCount}/>
+    </div>
+  )
+}
+
+export default Hero
