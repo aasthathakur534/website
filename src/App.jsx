2ad1288bc003d668c64551f804bbdeb2d8235c0e
aasthathakur534 <aasthakur1947@gmail.com>
2025-08-27 11:42:31 +0530

    feat: add background image+video

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-
+import { useState } from "react"
+import Background from "./components/Background/Background";
 const App = () => {
+  let heroData = [
+    {text1:"Drive into",text2: "what you love"},
+    {text1:"Indulge",text2: "your passions"},
+    {text1: "Give in to",text2:"your passions"},
+  ]
+  const [heroCount,setHeroCount] = useState(2);
+  const [playStatus,setPlayStatus] = useState(false);
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   )
 }
--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -6,7 +6,24 @@ import image3 from '../../Assets/image1.png'
 
 
 
-const Background = () => {
+const Background = ({playStatus,heroCount}) => {
+  if(playStatus) {
+    return (
+      <video className="background" autoPlay loop muted>
+        <source src={video1} type = 'video/mp4'/>
+      </video>
+    )
+  }
+  else if(heroCount===0){
+    return <img src={image1} className="background" alt="" />
+  }
+  else if(heroCount===1){
+    return <img src={image2} className="background" alt="" />
+  }
+  else if(heroCount===2){
+    return <img src={image3} className="background" alt="" />
+  }
+    
     
   return (
     <div>
